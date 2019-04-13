0c14e7f282b0880866626fb1d7bd89869106a892
elenweb <elenweb@narod.ru>
2019-04-14 00:54:44 +0300

    Звери

--- src/ru/itmonopoly/zveri/Main.java
+++ src/ru/itmonopoly/zveri/Main.java
@@ -1,12 +1,30 @@
 package ru.itmonopoly.zveri;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        Question root = new Question("лвет", null);
-        Zver dog = new Zver("собака", root);
-        root.yes = dog;
-        root.no = new Question("мяукает", root);
-        ((Question) root.no).no = new Zver("мышь", root.no);
-        ((Question) root.no).yes = new Zver("кошка", root.no);
+        Zver root = new Zver("лает", null, false);
+        root.yes = new Zver("хитрое", root, false);
+        root.yes.no = new Zver("собака", root.yes, true);
+        root.yes.yes = new Zver("рыжее", root.yes, false);
+        root.yes.yes.yes = new Zver("лиса", root.yes, true);
+        root.yes.yes.no = new Zver("шакал", root.yes, true);
+        root.no = new Zver("мяукает", root, false);
+        root.no.no = new Zver("живет в норке", root.no, false);
+        root.no.no.yes = new Zver("мышка", root.no, true);
+        root.no.no.no = new Zver("лошадь", root.no, true);
+        root.no.yes = new Zver("большое", root.no, false);
+        root.no.yes.yes = new Zver("лев", root.no, true);
+        root.no.yes.no = new Zver("кошка", root.no, true);
+        System.out.println("Задумайте животное");
+        try {
+            root.game(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- src/ru/itmonopoly/zveri/Zver.java
+++ src/ru/itmonopoly/zveri/Zver.java
@@ -1,10 +1,86 @@
 package ru.itmonopoly.zveri;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Zver {
     String name;
-    Object parent;
-    public Zver (String name, Object parent){
+    Zver parent;
+    Boolean type; // true - zver; false - question
+    Zver yes;
+    Zver no;
+
+
+    public Zver(String name, Zver parent, Boolean type) { //конструктор
         this.name = name;
         this.parent = parent;
+        this.type = type;
+    }
+
+    public void game(Zver z) throws IOException { //запуск игры
+        if (this.type) {
+            this.end(z);
+        } else {
+            this.question(z);
+        }
+    }
+
+    public void question(Zver z) throws IOException { //если z вопрос
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String answer;
+        System.out.println("Оно " + this.name + "? (+/-)");
+        answer = checkAnswer(br.readLine());
+            if (answer.equals("+")) {
+            if (this.yes.type) {
+                this.yes.end(z);
+            } else {
+                this.yes.question(z);
+            }
+        } else {
+            if (this.no.type) {
+                this.no.end(z);
+            } else {
+                this.no.question(z);
+            }
+        }
+    }
+
+    public void end(Zver z) throws IOException { //если z животное
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String answer;
+        System.out.println("Это " + this.name + "? (+/-)");
+        answer = checkAnswer(br.readLine());
+        if (answer.equals("+")) {
+            System.out.println("Угадала!");
+        } else {
+            if ((this.parent.no.type)&&!(this==this.parent.no)) {
+                this.parent.no.end(z);
+            } else {
+                System.out.println("Не угадала..."); //сюда пойдет метод добавления нового животного
+            }
+        }
+        System.out.println("Сыграем еще? (+/-)");
+        answer = checkAnswer(br.readLine());
+        if (answer.equals("-")) {
+            System.out.println("Спасибо за игру!");
+        } else {
+            z.game(z);
+        }
+    }
+
+    public static String checkAnswer(String s) throws IOException { // проверка ввода
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Boolean b = true;
+        while (b) {
+            if (s.equals("+") || s.equals("-")) {
+                return s;
+            } else {
+                System.out.println("Подходит только '+' или '-'. Повторите ввод пожалуйста.");
+                s = br.readLine();
+            }
+        }
+        return s;
     }
 }
+
